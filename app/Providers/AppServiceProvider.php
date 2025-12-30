836a9cd110e57401c798b6eda6d95751b90a3e81
rhann7 <raihandityam09@gmail.com>
2025-12-30 10:18:39 +0700

    feat(provider): add subfolder migration support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,19 +6,16 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $path = database_path('migrations');
+        $paths = array_merge([$path], glob($path . '/*', GLOB_ONLYDIR));
+
+        $this->loadMigrationsFrom($paths);
     }
 }
