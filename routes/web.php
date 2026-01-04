1abab412a234662fda394dbfa051669462c9ae13
rhann7 <raihandityam09@gmail.com>
2026-01-04 20:56:41 +0700

    feat(routes): implement routes for take/leave session

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
+Route::middleware(['auth'])->group(function () {
+    Route::impersonate();
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
