4b7af9c81dcb95b0f943b9d8d3788e5ff30e90cb
rhann7 <raihandityam09@gmail.com>
2026-01-04 18:30:37 +0700

    feat(user): add companyOwner relation to CompanyOwner model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     {
         return $this->hasRole('admin');
     }
+
+    public function companyOwner()
+    {
+        return $this->hasOne(CompanyOwner::class);
+    }
 }
